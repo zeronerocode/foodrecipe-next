5363f9ec35d30319f4b997b70d0fa20c4d23ec6f
Akmal Fadhilah <fadhilah@mhs.unsyiah.ac.id>
2022-08-05 09:07:23 +0700

    fixing bug update recipe

--- pages/recipe/update/[id].js
+++ pages/recipe/update/[id].js
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useCallback, useState, useEffect } from 'react'
 import style from "../Create.module.css"
 import Head from "next/head";
@@ -5,93 +6,144 @@ import Layout from "../../../components/layout/Layout";
 import Input from "../../../components/base/Input"
 import Textarea from "../../../components/base/Textarea";
 import Button from "../../../components/base/Button";
-import Tiptap from '../../../components/base/Tiptap';
-import { useRouter } from 'next/router'
+import { Editor, EditorState } from "draft-js";
+import "draft-js/dist/Draft.css";
+import { useRouter} from 'next/router'
 import axios from 'axios'
+import Swal from 'sweetalert2';
 
-const Update = (recipeDetail) => {
+const Update = () => {
   const router = useRouter()
-  const [files, setFiles] = useState([])
-  const [video, setVideo] = useState('')
-  const [image, setImage] = useState('')
-  const [recipe, setRecipe] = useState({
-    title: '',
-    ingredients: ''
+  const id = router.query.id
+  const [dataRecipe, setDataRecipe] = useState({
+    title: "",
+    ingredients: "",
+    image: "",
+    video: ""
   })
-  const {id} = recipeDetail.recipeDetail[0]
-  const resep = recipeDetail.recipeDetail[0]
-  // image = recipeDetail.recipeDetail[0].recipe_photo
-  // video = recipeDetail.recipeDetail[0].recipe_video
-  console.log("resep update=>",resep)
-  console.log("image => ", image);
-  const hanldeImageUpload = (e) => {
-    console.log(e.target.files[0]);
-    let upload = e.target.files[0]
-    setImage(upload)
+
+  console.log('data =>', dataRecipe);
+
+  const [title, setTitle] =  useState("")
+  const [videoTitle, setVideoTitle] = useState("")
+  const handleChange = (e) => {
+    setDataRecipe({
+      ...dataRecipe,
+      [e.target.name]: [e.target.value]
+    })
   }
 
-  const handleVideoUpload = (e) => {
-    console.log(e.target.files[0]);
-    let upload = e.target.files[0]
-    setVideo(upload)
+  const [imagePriview, setImagePriview] = useState("")
+
+  const imageUpload = (e) => {
+    const file = e.target.files[0]
+    setDataRecipe({
+      ...dataRecipe,
+      image: file
+    })
+    setImagePriview(URL.createObjectURL(file))
   }
 
-  const handleInput = (e) => {
-    e.persist();
-    setRecipe({ ...recipe, [e.target.name]: e.target.value })
+  const videoUpload = (e) => {
+    const file = e.target.files[0]
+    setDataRecipe({
+      ...dataRecipe,
+      video: file
+    })
   }
 
-
-  const handleSubmit = async (e) => {
-    e.preventDefault()
-    let formData = new FormData()
-    formData.append('recipePhoto', image)
-    formData.append('recipeVideo', video)
-
-    formData.append('title', recipe.title)
-    formData.append('ingredients', recipe.ingredients)
-
+  async function fetchData (dataform, id) {
     try {
-      const result = await axios.put(`${process.env.NEXT_APP_API_URL}/recipe/${id}`, formData)
-      router.push(`/`)
+      const result = await axios.put(`${process.env.NEXT_APP_API_URL}/recipe/${id}`, dataform, {
+        headers: {
+          "content-type": "multipart/form-data"
+        },
+      })
+      const recipe = result.data.data
+      Swal.fire({
+        position: "top-end",
+        icon: "success",
+        title: "Update Recipe Success",
+        showConfirmButton: false,
+        timer: 1500,
+      });
+      router.push(`/recipe/${id}`)
+      console.log('recipes =>',recipe);
     } catch (error) {
-
+      Swal.fire({
+        position: "top-end",
+        icon: "success",
+        title: "Update Recipe Success",
+        showConfirmButton: false,
+        timer: 1500,
+      });
+      console.log(error);
     }
   }
 
+  const handleUpdateRecipe = (e) => {
+    const data = new FormData()
+    data.append('title', dataRecipe.title)
+    data.append('recipePhoto', dataRecipe.image)
+    data.append('ingredients', dataRecipe.ingredients)
+    data.append('recipeVideo', dataRecipe.video)
+    e.preventDefault()
+    fetchData(data, id)
+  }
+
+  async function fetchDataId(id){
+    try {
+      const result = await axios.get(`${process.env.NEXT_APP_API_URL}/recipe/${id}`)
+      const recipes = result.data.data
+      console.log('recipes =>',recipes[0]);
+      setDataRecipe({
+        ...dataRecipe,
+        title: recipes[0].title,
+        ingredients: recipes[0].ingredients
+      })
+      setImagePriview(recipes.image)
+      setVideoTitle(recipes.video)
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  useEffect(()=>{
+    fetchDataId(id)
+  },[id])
   return (
     <>
       <Layout title='Update Recipe - FoodRecipe'>
         <div className="container mt-5 text-center">
-          <form onSubmit={handleSubmit}>
+          <form onSubmit={handleUpdateRecipe}>
             <Input
               type={'file'}
               name={'recipe_image'}
               className={style.imgInput}
-              onChange={hanldeImageUpload}
-              defaultValue={image || ''}
+              onChange={(e)=>imageUpload(e)}
+              value={''}
               placeholder={'add photo'}>
             </Input>
             <Input
               className={style.formInput}
               type={'text'}
               name={'title'}
-              onChange={handleInput}
-              defaultValue={resep.title}
+              onChange={handleChange}
+              value={dataRecipe.title}
               placeholder={'Title'} />
             <Textarea
               className={style.areaInput}
               name={'ingredients'}
-              onChange={handleInput}
-              defaultValue={resep.ingredients}
+              onChange={handleChange}
+              value={dataRecipe.ingredients}
               placeholder={'Ingredients'}>
             </Textarea>
             <Input type={'file'}
               className={style.videoInput}
               name={'recipe_video'}
-              onChange={handleVideoUpload}
+              onChange={(e)=>videoUpload(e)}
               placeholder={'Video'} 
-              defaultValue={resep.recipe_video || ''}
+              value={''}
               />
               
             <Button type='submit' className={style.btnPost}>Post</Button>
@@ -101,25 +153,25 @@ const Update = (recipeDetail) => {
     </>
   );
 };
-export const getServerSideProps = async (context) => {
-  // const cookie = context.req.headers.cookie
-  const { id } = context.params
-  // if (!cookie) {
-  //   // Router.replace('/login')
-  //   context.res.writeHead(302, {
-  //     Location: `http://localhost:3000/auth/login`
-  //   })
-  //   return {}
-  // }
-  const { data: RespData } = await axios.get(`${process.env.NEXT_APP_API_URL}/recipe/${id}`, {
-    headers: {
-    }
-  });
-  // console.log(RespData);
-  return {
-    props: {
-      recipeDetail: RespData.data,
-    }, // will be passed to the page component as props
-  };
-}
+// export const getServerSideProps = async (context) => {
+//   // const cookie = context.req.headers.cookie
+//   const { id } = context.params
+//   // if (!cookie) {
+//   //   // Router.replace('/login')
+//   //   context.res.writeHead(302, {
+//   //     Location: `http://localhost:3000/auth/login`
+//   //   })
+//   //   return {}
+//   // }
+//   const { data: RespData } = await axios.get(`${process.env.NEXT_APP_API_URL}/recipe/${id}`, {
+//     headers: {
+//     }
+//   });
+//   // console.log(RespData);
+//   return {
+//     props: {
+//       recipeDetail: RespData.data,
+//     }, // will be passed to the page component as props
+//   };
+// }
 export default Update;
