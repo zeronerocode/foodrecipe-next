d67735905b51f39d9f49af92642e0359581a4605
Akmal Fadhilah <fadhilah@mhs.unsyiah.ac.id>
2022-08-09 12:50:49 +0700

    screenshoot

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   env: {
     TITLE_APP: 'food recipe',
-    NEXT_APP_API_URL: 'https://food-recipe-server.herokuapp.com/v1',
+   NEXT_APP_API_URL: 'https://food-recipe-server.herokuapp.com/v1',
     // NEXT_APP_API_URL: 'http://localhost:5000/v1'
   },
   reactStrictMode: true,
